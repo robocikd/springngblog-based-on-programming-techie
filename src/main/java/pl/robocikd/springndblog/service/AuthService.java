76976c3100ec2afd2397a4c1d4af7af9fc1ee1df
Damian <robocikd@wp.pl>
2021-04-26 17:24:43 +0200

    BCryptPasswordEncoder

--- src/main/java/pl/robocikd/springndblog/service/AuthService.java
+++ src/main/java/pl/robocikd/springndblog/service/AuthService.java
@@ -1,5 +1,6 @@
 package pl.robocikd.springndblog.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.robocikd.springndblog.dto.RegisterRequest;
 import pl.robocikd.springndblog.model.User;
@@ -9,17 +10,23 @@ import pl.robocikd.springndblog.repository.UserRepository;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService(UserRepository userRepository) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
 
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
